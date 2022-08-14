013ec106d7cfd9d80014a511b35215464d900164
RubixDev <silas.groh@t-online.de>
2022-08-14 18:43:42 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+cd dist
+
+# set custom domain
+echo enchantedshulkers.rubixdev.de > CNAME
+
+# deploy
+git init -b master
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:RubixDev/enchantedshulkers-pack-patcher.git master:gh-pages
+
+cd -
